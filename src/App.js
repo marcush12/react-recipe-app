955422d88a1d4917e845630a5658e927c5431ce3
marcush12 <marcos012santos@gmail.com>
2019-06-03 11:44:56 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>Hello from our recipe app</div>
+  );
+}
+
+export default App;
